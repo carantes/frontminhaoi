81c85fc50953ea42b3068849ac4a6c04556683ac
oi394271 <nadia.trigo@oi.net.br>
2016-09-26 10:29:05 -0300

    versão limpa de lixo

--- dist/server-build/config/index.js
+++
@@ -1,7 +0,0 @@
-let config = {
-  API_BASE_URL_EXTERNAL: process.env.API_BASE_URL_EXTERNAL || 'http://api.teste.com:8091/api'
-  // API_BASE_URL_EXTERNAL: process.env.API_BASE_URL_EXTERNAL || 'http://dadhx03.interno:8091/api'
-  , API_BASE_URL_INTERNAL: process.env.API_BASE_URL_INTERNAL || 'http://localhost:3000' 
-}
-
-export default config;
