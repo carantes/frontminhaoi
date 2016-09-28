b765b163bc2981406daeb27ced198cf11db7b934
oi394271 <nadia.trigo@oi.net.br>
2016-09-28 11:30:44 -0300

    mudança no endpoint. Apontando para homologação.

--- transicao/integracao/js/init.js
+++ transicao/integracao/js/init.js
@@ -4,7 +4,7 @@
 
     $('#redirectButton').on( "click", function(e) {
       e.preventDefault();
-      $.post( "http://localhost:8091/api/migrarprecadastro", { cpf: "07797441790" }, function( data ){       
+      $.post( "http://dadhx03.interno:8091/api/migrarprecadastro", { cpf: "07797441790" }, function( data ){       
         //redirecionar
          if (data.redirect) {
            // data.redirect contains the string URL to redirect to
